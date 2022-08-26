de54868a7ed47780bb62aff54d4a1b95752dd3ff
RasmusAnkar <rasmus.ankar@elev.ga.ntig.se>
2022-08-26 14:27:06 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+
+namespace repetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Uppgift 1
+            Console.WriteLine("Hello World");
+            */
+
+            /* Uppgift 2
+            Console.WriteLine("Once upon a time");
+            Console.WriteLine("I was falling in love");
+            Console.WriteLine("I was falling in love");
+            */
+
+            /* Uppgift 3 
+            Console.WriteLine(" Once upon a time \n I was falling in loveNow \n I’m only falling apart");
+            */
+
+            /* Uppgift 4
+            String text = "Live and sleep";
+            Console.WriteLine(text
+            */
+
+            /* Uppgift 5
+            string name = "ada lovlace";
+            Console.WriteLine("hej " + name);
+            */
+            /* Uppgift 6
+            Console.WriteLine("Give input");
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+            */
+            /* Uppgift 7
+            Console.WriteLine("Give input");
+            string input2 = Console.ReadLine();
+            Console.WriteLine(input2);
+            Console.WriteLine(input2);
+            Console.WriteLine(input2);
+            */
+            /* Uppgift 8
+            Console.WriteLine("What is your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("hello " + name + "!");
+            */
+            /* Uppgift 9
+            Console.WriteLine("Hello how are you");
+            Console.ReadLine();
+            Console.WriteLine("how interesting tell me more");
+            Console.ReadLine();
+            Console.WriteLine("thank you for sharing");
+            */
+
+            /* Uppgift 10
+            Console.WriteLine("Tell me a name");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("Tell me a profession");
+            string profession = Console.ReadLine();
+            Console.WriteLine("There once was a " + profession + " Named " + name2 + " Who went to the store to buy apples");
+            */
+            /* Uppgift 11
+            int days = 200;
+            double hours = 3.5;
+            string coding = "Coding is fun";
+            Console.WriteLine("Days to summer:");
+            Console.WriteLine(days);
+            Console.WriteLine("Hours to lunch:");
+            Console.WriteLine(hours);
+            Console.WriteLine("Coding is fun:");
+            Console.WriteLine(coding);
+            */
+
+            /* Uppgift 12
+            Console.WriteLine("Give me a number");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("you gave " + num);
+            */
+
+            /* Uppgift 13
+            Console.WriteLine("Give me a number");
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("you gave " + num2);
+            */
+
+            /* Uppgift 14
+            Console.WriteLine("give me the truth");
+            bool svar = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("svar");
+            */
+
+            /* Uppgift 15
+            Console.WriteLine("give me a string");
+            string word = Console.ReadLine();
+            Console.WriteLine("give me a integer");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("give me a double");
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("give me an boolean");
+            bool answer = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Your string " + word);
+            Console.WriteLine("your integer " + num);
+            Console.WriteLine("your double " + num2);
+            Console.WriteLine("your bool " + answer);
+            */
+            /* Uppgift 16
+            Console.WriteLine("How many days");
+            int days = int.Parse(Console.ReadLine());
+            int seconds = days * 86400;
+            Console.WriteLine(seconds);
+            */
+
+            /* Uppgift 17
+            Console.WriteLine("give me the first number");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("give me the second number");
+            int num2 = int.Parse(Console.ReadLine());
+            int sum = num + num2;
+            Console.WriteLine("the sum is " + sum);
+            */
+
+            /* Uppgift 18
+            Console.WriteLine("give me the first number");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("give me the second number");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("give me the third number");
+            int num3 = int.Parse(Console.ReadLine());
+            int sum = num + num2 + num3;
+            Console.WriteLine("the sum is " + sum);
+            */
+            /* Uppgift 19
+            Console.WriteLine("give me the first number");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("give me the second number");
+            int num2 = int.Parse(Console.ReadLine());
+            int sum = num + num2;
+            Console.WriteLine( num + "+" + num2 + "=" + sum);
+            */
+            /* Uppgift 20
+            Console.WriteLine("give me the first number");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("give me the second number");
+            int num2 = int.Parse(Console.ReadLine());
+            int sum = num*num2;
+            Console.WriteLine("the product is " + sum);
+            */
+            /* Uppgift 21*/
+            Console.WriteLine("give me the first number");
+            double num = double.Parse(Console.ReadLine());
+            Console.WriteLine("give me the second number");
+            double num2 = double.Parse(Console.ReadLine());
+            double avrage = (num + num2)/2;
+            Console.WriteLine("the avrage is " + avrage);
+
+            /* Uppgift 17*/
+
+            /* Uppgift 17*/
+
+            /* Uppgift 17*/
+
+            /* Uppgift 17*/
+
+            /* Uppgift 17*/
+
+            /* Uppgift 17*/
+
+            /* Uppgift 17*/
+
+            /* Uppgift 17*/
+
+            /* Uppgift 17*/
+
+            /* Uppgift 17*/
+
+            /* Uppgift 17*/
+        }
+    }
+}
